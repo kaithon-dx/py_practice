8b91df649172b570f15a955b079ce2427d9764df
kai <kairug713@gmail.com>
2026-01-24 18:12:57 +0900

    最初のプログラム（挨拶して名前と年齢を尋ねるもの）を追記

--- c1.py
+++ c1.py
@@ -11,3 +11,14 @@ c_int = int(c)
 print("a_float:", a_float)  # 出力: a_float: 10.0
 print("b_int:", b_int) # 出力: b_int: 3
 print("c_int:", c_int) # 出力: c_int: 42
+
+# 最初のプログラム：挨拶を表示して名前と年齢を尋ねる
+print('Hello, World!')
+print('What is your name?')
+my_name = input()
+print('It is good to meet you, ' + my_name)
+print('The length of your name is:')
+print(len(my_name))
+print('What is your age?')
+my_age = input()
+print('You will be ' + str(int(my_age) + 1) + ' in a year.')
